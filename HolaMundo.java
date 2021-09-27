603ea5061db923d7b371ac14995d2c22d89b0a66
miguelnunezcaro <alum.mnunez.2021@iesalixar.org>
2021-09-27 14:12:35 +0200

    comentarios añadidos al cófigo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,8 @@
+/**
+* Muestra por pantalla ¡Hola Mundo!
+*
+* @author miguelnunezcaro
+**/
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
