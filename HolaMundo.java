42a2addf04eead469081d054a45b1da8bc3faa85
miguelnunezcaro <alum.mnunez.2021@iesalixar.org>
2021-10-04 13:16:02 +0200

    Saludo en Colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -8,8 +8,8 @@ public class HolaMundo {
     	String azul = "/033[34m";
     
     public static void main(String[] args) {
-   	System.out.println("Hola Mundo");
-   	System.otu.println("Aquí estamos haciendo pruebas con Git");
+   	System.out.println(naranja+"Hola Mundo");
+   	System.otu.println(azul+"Aquí estamos haciendo pruebas con Git");
     }
 }
 
