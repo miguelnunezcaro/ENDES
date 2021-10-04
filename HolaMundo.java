2c3fb5536b1b82ae4c5750592a5de19615d5e11d
miguelnunezcaro <alum.mnunez.2021@iesalixar.org>
2021-10-04 13:08:37 +0200

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,6 +4,9 @@
 * @author miguelnunezcaro
 **/
 public class HolaMundo {
+    	String naranja = "/033[33m";
+    	String azul = "/033[34m";
+    
     public static void main(String[] args) {
    	System.out.println("Hola Mundo");
    	System.otu.println("Aquí estamos haciendo pruebas con Git");
